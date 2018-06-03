cc6c76822c59aa2ecaa8bdfcf376cadac6ee0728
Michael Gill <michaelgill8123@outlook.com>
2018-06-03 16:45:33 -0700

    added things

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+python_installer- a package for creating installers with python.
+
+project started: June 3, 2018
+"""
+
+__author__ = "Michael Gill <michaelveenstra12@gmail.com>"
+__version__ = '0.0a'
