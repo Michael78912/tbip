fe96faad115c26a9283a43472bbcc81c7e0ec98f
Michael Gill <michael8123@outlook.com>
2018-08-16 17:13:50 -0700

    add setup.py for PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst") as openfile:
+    long_description = openfile.read()
+
+setuptools.setup(
+    name="tbip",
+    version="0.0.0.1",
+    author="Michael Gill",
+    author_email="michael.78912.8@gmail.com",
+    description="Build cross-platform installers",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/Michael78912/tbip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
